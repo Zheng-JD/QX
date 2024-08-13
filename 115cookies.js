648ff8f342d9eb8e60920768456f720b6c969689
我也爱你们 <108261457+Zheng-JD@users.noreply.github.com>
2024-08-13 16:19:53 +0800

    Update 115cookies.js

--- 115cookies.js
+++ 115cookies.js
@@ -1,14 +1,28 @@
 // 115cookies.js
 const cookieName = '115'
 const cookieKey = 'chen_115_cookie'
+const notifyKey = 'chen_115_cookie_notify'
 const chen = init()
 const cookieVal = $request.headers['Cookie']
+
 if (cookieVal) {
   if (chen.setdata(cookieVal, cookieKey)) {
-    chen.msg(`${cookieName}`, '获取Cookie: 成功', '')
-    chen.log(`[${cookieName}] 获取Cookie: 成功, cookie: ${cookieVal}`)
+    const oldNotifyTime = chen.getdata(notifyKey)
+    const currentTime = new Date().getTime()
+    
+    // 检查是否在最近5分钟内已经发送过通知
+    if (!oldNotifyTime || currentTime - oldNotifyTime > 5 * 60 * 1000) {
+      chen.msg(`${cookieName}`, '获取Cookie: 成功', '')
+      chen.log(`[${cookieName}] 获取Cookie: 成功, cookie: ${cookieVal}`)
+      
+      // 更新最后通知时间
+      chen.setdata(currentTime.toString(), notifyKey)
+    } else {
+      chen.log(`[${cookieName}] 获取Cookie: 成功, 但在5分钟内已经通知过，不再重复通知`)
+    }
   }
 }
+
 function init() {
   isSurge = () => {
     return undefined === this.$httpClient ? false : true
