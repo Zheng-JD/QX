c73619d9f106a082729cf47983a9d98ad5ad786b
我也爱你们 <108261457+Zheng-JD@users.noreply.github.com>
2024-08-13 15:36:53 +0800

    Update get115Cookies.js

--- get115Cookies.js
+++ get115Cookies.js
@@ -1,12 +1,6 @@
-```javascript
-^https?:\/\/passportapi\.115\.com\/app\/1\.0\/web\/\w+\/login url script-response-body https://raw.githubusercontent.com/Zheng-JD/QX/main/get115Cookies.js
-
 [rewrite_local]
-^https?:\/\/passportapi\.115\.com\/app\/1\.0\/web\/\w+\/login url script-response-body get115Cookies.js
+# 115生活App 获取 Cookies
+^https:\/\/webapi\.115\.com\/user\/signin.*$ url script-request-header 115_get_cookie.js
 
 [mitm]
-hostname = passportapi.115.com
-
-[script]
-get115Cookies.js = type=http-response,pattern=^https?:\/\/passportapi\.115\.com\/app\/1\.0\/web\/\w+\/login,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/your_repo/get115Cookies.js
-```
+hostname = webapi.115.com
