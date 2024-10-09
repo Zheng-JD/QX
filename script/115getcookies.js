9df4a82a420423942edb70a56df06b024632fd0a
一口气吃掉好几个小朋友 <108261457+Zheng-JD@users.noreply.github.com>
2024-10-09 10:40:51 +0800

    Create 115getcookies.js

---
+++ script/115getcookies.js
@@ -0,0 +1,15 @@
+#!name=115 Cookie获取
+#!desc=自动获取115的Cookie
+#!author=Assistant
+#!homepage=https://github.com/your_username/your_repo
+#!icon=https://raw.githubusercontent.com/your_username/your_repo/main/115.png
+
+[Script]
+http-request https:\/\/my\.115\.com\/proapi\/3\.0\/index\.php script-path=https://raw.githubusercontent.com/Zheng-JD/X/refs/heads/main/script/115.js, requires-body=true, tag=115Cookie获取
+
+[MITM]
+hostname = my.115.com
+
+[Script]
+# 115 Cookie获取
+115_cookie = type=http-request, pattern=https:\/\/my\.115\.com\/proapi\/3\.0\/index\.php, script-path=https://raw.githubusercontent.com/Zheng-JD/X/refs/heads/main/script/115.js, requires-body=true, tag=115Cookie获取
