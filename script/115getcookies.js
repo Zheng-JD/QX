1dc6e95eaaf015d81dadfbb6ffe4b5f1cbdf182b
一口气吃掉好几个小朋友 <108261457+Zheng-JD@users.noreply.github.com>
2024-10-09 10:42:44 +0800

    Update 115getcookies.js

--- script/115getcookies.js
+++ script/115getcookies.js
@@ -5,11 +5,11 @@
 #!icon=https://raw.githubusercontent.com/your_username/your_repo/main/115.png
 
 [Script]
-http-request https:\/\/my\.115\.com\/proapi\/3\.0\/index\.php script-path=https://raw.githubusercontent.com/Zheng-JD/X/refs/heads/main/script/115.js, requires-body=true, tag=115Cookie获取
+http-request https:\/\/my\.115\.com\/proapi\/3\.0\/index\.php script-path=https://raw.githubusercontent.com/Zheng-JD/X/refs/heads/main/script/115cookies.js, requires-body=true, tag=115Cookie获取
 
 [MITM]
 hostname = my.115.com
 
 [Script]
 # 115 Cookie获取
-115_cookie = type=http-request, pattern=https:\/\/my\.115\.com\/proapi\/3\.0\/index\.php, script-path=https://raw.githubusercontent.com/Zheng-JD/X/refs/heads/main/script/115.js, requires-body=true, tag=115Cookie获取
+115_cookie = type=http-request, pattern=https:\/\/my\.115\.com\/proapi\/3\.0\/index\.php, script-path=https://raw.githubusercontent.com/Zheng-JD/X/refs/heads/main/script/115cookies.js, requires-body=true, tag=115Cookie获取
